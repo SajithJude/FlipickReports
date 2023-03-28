617646a730598f40e7412f416839706993dc0a1a
Jude Sajith <judesajith.aj@gmail.com>
2023-03-28 03:44:48 -0400

    jjj

--- pages/Predictive_Analytics.py
+++ pages/Predictive_Analytics.py
@@ -11,10 +11,10 @@ df_activity  = st.session_state['df_activity']
 # st.table(df_levelwise_assessment.head())
 # st.table(df_enrollment_metrics.head())
 
-st.write(df_activity.columns[2])
+st.write(df_activity.iloc[:,3])
 # Merge the dataframes
 # Merge the dataframes using iloc to select columns 1 and 2
-# df = pd.merge(df_activity, df_levelwise_assessment, on=[df_activity.columns[2], df_activity.columns[3]])
-# df = pd.merge(df, df_enrollment_metrics, on=[df_activity.columns[2], df_activity.columns[3]])
+df = pd.merge(df_activity, df_levelwise_assessment, on=[df_activity.iloc[:,2], df_activity.iloc[:,3]])
+df = pd.merge(df, df_enrollment_metrics, on=[df_activity.iloc[:,2], df_activity.iloc[:,3]])
 
 st.table(df_activity.head())
