58a0260672ab276465b76f8319372b5831ac90fe
Jude Sajith <judesajith.aj@gmail.com>
2023-03-28 04:09:26 -0400

    ssss

--- pages/Predictive_Analytics.py
+++ pages/Predictive_Analytics.py
@@ -7,6 +7,12 @@ df_levelwise_assessment  = st.session_state['df_levelwise_assessment']
 df_enrollment_metrics  = st.session_state['df_enrollment_metrics'] 
 df_activity  = st.session_state['df_activity'] 
 
+
+df_levelwise_assessment.columns = [c.replace(' ', '_') for c in df_levelwise_assessment.columns]
+df_enrollment_metrics.columns = [c.replace(' ', '_') for c in df_enrollment_metrics.columns]
+df_activity.columns = [c.replace(' ', '_') for c in df_activity.columns]
+
+
 st.table(df_activity.head())
 # st.table(df_levelwise_assessment.head())
 # st.table(df_enrollment_metrics.head())
