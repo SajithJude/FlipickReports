7148ff00d519c5ff0341fb1ae24f7b9c6084ff52
Jude Sajith <judesajith.aj@gmail.com>
2023-03-28 07:56:27 -0400

    ssss

--- pages/Predictive_Analytics.py
+++ pages/Predictive_Analytics.py
@@ -19,23 +19,29 @@ df_enrollment_metrics['target'] = df_enrollment_metrics.apply(lambda row: 1 if r
 df_enrollment_metrics['Enrollment_Date'] = pd.to_datetime(df_enrollment_metrics['Enrollment_Date'])
 df_enrollment_metrics['DaysCount_after_enrolling'] = (pd.to_datetime('today') - df_enrollment_metrics['Enrollment_Date']).dt.days
 
+averagedayscount = df_enrollment_metrics['DaysCount_after_enrolling'].mean()
+
+df_filtered = df_enrollment_metrics.loc[(df_enrollment_metrics['target'] == 1) & (df_enrollment_metrics['DaysCount_after_enrolling'] >= averagedayscount)]
+
+# Select the columns with user names and emails
+df_filtered = df_filtered[['Learner Name', 'Email Id']]
+
+# Show the filtered dataframe
+st.table(df_filtered)
 
 
-st.table(df_activity.head())
-st.table(df_enrollment_metrics.head())
+# st.table(df_activity.head())
+# st.table(df_enrollment_metrics.head())
 
 
-df = pd.merge(df_activity, df_levelwise_assessment, on=['Learner_Name', 'Email_Id'])
+# df = pd.merge(df_activity, df_levelwise_assessment, on=['Learner_Name', 'Email_Id'])
 
-st.table(df.head())
-# df2 = pd.merge(df, df_enrollment_metrics, on=['Learner_Name'])
+# st.table(df.head())
 
-
-# st.table(df2.head())
-df = df.drop(['Institute_x', 'Enrolled_By', 'Enrollment_Date'], axis=1)
+# df = df.drop(['Institute_x', 'Enrolled_By', 'Enrollment_Date'], axis=1)
 
 # # Merge the dataframes using iloc to select columns 1 and 2
 # df = pd.merge(df_activity, df_levelwise_assessment, on=[df_activity.iloc[:,1], df_activity.iloc[:,2]])
 # df = pd.merge(df, df_enrollment_metrics, on=[df_activity.iloc[:,1], df_activity.iloc[:,2]])
 
-st.table(df.head())
+# st.table(df.head())
