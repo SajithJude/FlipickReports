5f8642fa9f6032921351b1d18ceb4e2b2129c554
Jude Sajith <judesajith.aj@gmail.com>
2023-03-28 12:07:31 -0400

    sss

--- pages/Predictive_Analytics.py
+++ pages/Predictive_Analytics.py
@@ -28,9 +28,11 @@ df_levelwise_assessment.columns = [c.replace(' ', '_') for c in df_levelwise_ass
 df_enrollment_metrics.columns = [c.replace(' ', '_') for c in df_enrollment_metrics.columns]
 df_activity.columns = [c.replace(' ', '_') for c in df_activity.columns]
 
+
+df_enrollment_metrics = df_enrollment_metrics.rename(columns={'Learner_Email': 'Email_Id'})
+
 df_enrollment_metrics['target'] = df_enrollment_metrics.apply(lambda row: 1 if row['Diagnostic_Or_First_Level_Assigned'] == row['Current_Level'] else 0, axis=1)
 # df_enrollment_metrics['DaysCount_after_enrolling'] = str( pd.to_datetime('today') - df_enrollment_metrics['Enrollment_Date']).dt.days
-df_enrollment_metrics = df_enrollment_metrics.rename(columns={'Learner_Email': 'Email_Id'})
 
 df_enrollment_metrics['Enrollment_Date'] = pd.to_datetime(df_enrollment_metrics['Enrollment_Date'])
 df_enrollment_metrics['DaysCount_after_enrolling'] = (pd.to_datetime('today') - df_enrollment_metrics['Enrollment_Date']).dt.days
