f2d5b12891e23947f9cd6bb7114ced3730d17a72
Jude Sajith <judesajith.aj@gmail.com>
2023-03-28 03:34:52 -0400

    ssss

--- pages/Predictive_Analytics.py
+++ pages/Predictive_Analytics.py
@@ -6,9 +6,9 @@ df_levelwise_assessment  = st.session_state['df_levelwise_assessment']
 df_enrollment_metrics  = st.session_state['df_enrollment_metrics'] 
 df_activity  = st.session_state['df_activity'] 
 
-st.table(df_activity.head())
-st.table(df_levelwise_assessment.head())
-st.table(df_enrollment_metrics.head())
+# st.table(df_activity.head())
+# st.table(df_levelwise_assessment.head())
+# st.table(df_enrollment_metrics.head())
 
 
 # Merge the dataframes
