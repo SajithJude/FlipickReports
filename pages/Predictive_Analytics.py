b692398dc83712382b40acc0dae1b64606d18f30
Jude Sajith <judesajith.aj@gmail.com>
2023-03-28 04:03:29 -0400

    ssss

--- pages/Predictive_Analytics.py
+++ pages/Predictive_Analytics.py
@@ -13,8 +13,8 @@ st.table(df_activity.head())
 
 st.write(df_activity.iloc[:, 1])
 # Merge the dataframes
-df = pd.merge(df_activity, df_levelwise_assessment, on=[df_activity.columns[1], df_activity.columns[2]])
-df = pd.merge(df, df_enrollment_metrics, on=[df_activity.columns[1], df_activity.columns[2]])
+df = pd.merge(df_activity, df_levelwise_assessment, on=[`Learner Name`, `Email Id`])
+df = pd.merge(df, df_enrollment_metrics, on=['Learner Name', 'Email Id'])
 
 # # Merge the dataframes using iloc to select columns 1 and 2
 # df = pd.merge(df_activity, df_levelwise_assessment, on=[df_activity.iloc[:,1], df_activity.iloc[:,2]])
