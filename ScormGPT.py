2d9d9d0e243716bf244de3ac769f50743df71505
Jude Sajith <judesajith.aj@gmail.com>
2023-03-28 00:22:37 -0400

    snsns

--- ScormGPT.py
+++ ScormGPT.py
@@ -1,93 +1,35 @@
+import pandas as pd
 import streamlit as st
-import zipfile
-import os
+import matplotlib.pyplot as plt
+import mpld3
+from mpld3 import plugins
+import streamlit.components.v1 as components
 
+# Read data from CSV files
+df_activity = pd.read_csv('ALL level activity.csv')
+df_levelwise_assessment = pd.read_csv('Level Wise assessment for level1.csv')
+df_enrollment_metrics = pd.read_csv('EnrollmentMetrics.csv')
 
+# Calculate performance metrics
+total_learners = len(df_enrollment_metrics)
+total_attempts = df_activity['Total No Of Attempts'].sum()
+average_time_spent = df_activity['Total Time Spent'].mean()
 
+# Create a bar chart of total attempts for each module
+fig, ax = plt.subplots()
+df_activity[['Modules Completed', 'Total No Of Attempts']].groupby(['Modules Completed']).sum().plot(kind='bar', ax=ax)
+ax.set_xlabel('Modules Completed')
+ax.set_ylabel('Total No Of Attempts')
+plt.title('Total Attempts by Module')
 
+# Convert the plot to an interactive chart using mpld3
+html_graph = mpld3.fig_to_html(fig, template_type="simple")
 
-
-# import streamlit as st
-import openai 
-from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader,PromptHelper,QuestionAnswerPrompt
-import os 
-from streamlit_chat import message as st_message
-
-# favicon = "favicon.ac8d93a.69085235180674d80d902fdc4b848d0b.png"
-# st.set_page_config(page_title="Flipick Chat", page_icon=favicon)
-
-openai.api_key = os.getenv("API_KEY")
-
-
-# Define the function to extract the zip file and save the contents to a directory
-def extract_zipfile(zip_file, save_directory):
-    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-        zip_ref.extractall(save_directory)
-
-# Create a file uploader component for the zip file
-zip_file = st.file_uploader('Upload a zip file', type='zip')
-
-# If the user uploads a file, extract its contents and save them to a directory
-if zip_file is not None:
-    # Create a directory for the extracted files
-    save_directory = 'scorm'
-    if not os.path.exists(save_directory):
-        os.makedirs(save_directory)
-    
-    # Call the extract_zipfile function to extract the zip file and save the contents to the directory
-    extract_zipfile(zip_file, save_directory)
-    
-    # Display a success message
-    st.success('Zip file extracted and saved to directory: {}'.format(save_directory))
-
-
-try:
-    index = GPTSimpleVectorIndex.load_from_disk('indx.json')
-except FileNotFoundError:
-    # Loading from a directory
-    documents = SimpleDirectoryReader('scorm').load_data()
-    index = GPTSimpleVectorIndex(documents)
-    index.save_to_disk('indx.json')
-
-if "history" not in st.session_state:
-    st.session_state.history = []
-
-def generate_answer():
-    user_message = st.session_state.input_text
-    
-    if any(op in user_message for op in ['+', '-', '*', '/', '%']):
-        st.session_state.history.append({"message": user_message, "is_user": True})
-        st.session_state.history.append({"message": "I'm sorry, I'm not allowed to perform calculations.", "is_user": False})
-    else:
-        query_str = str(user_message)
-        context_str = "Generate answers for the questions that are relevant only to the documents context, throw a default answer saying I dont know for unrelevant questions."
-        QA_PROMPT_TMPL = (
-            "We have provided context information below. \n"
-            "---------------------\n"
-            "{context_str}"
-            "\n---------------------\n"
-            "Given this information, please answer the question: {query_str}\n"
-        )
-        QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
-        message_bot = index.query(query_str, text_qa_template=QA_PROMPT, response_mode="compact", mode="embedding")
-        # source = message_bot.get_formatted_sources()
-        # st.sidebar.write("Answer Source :",source)  # added line to display source on sidebar
-        st.session_state.history.append({"message": user_message, "is_user": True})
-        st.session_state.history.append({"message": str(message_bot), "is_user": False})
-
-# col1, col2 = st.columns([1.4, 1])
-# col2.image("Flipick_Logo-1.jpg", width=210)
-st.write("")
-st.write("")
-
-input_text = st.text_input("Ask Chatbot a question", key="input_text", on_change=generate_answer)
-# st.caption("Disclaimer : This ChatBOT is a pilot built solely for the purpose of a demo to Indian Institute of Banking and Finance (IIBF). The BOT has been trained based on the book Treasury Management published by IIBF. All content rights vest with IIBF")
-
-# Display the conversation history
-for chat in st.session_state.history:
-    st_message(**chat)
-
-
-
-# # Set up the Streamlit app
-# st.title('Zip File Uploader')
+# Create the streamlit app
+st.title('Overall Performance Dashboard')
+st.subheader('Performance Metrics')
+st.write('Total Number of Learners:', total_learners)
+st.write('Total Number of Attempts:', total_attempts)
+st.write('Average Time Spent:', average_time_spent)
+st.subheader('Total Attempts by Module')
+components.html(html_graph)
