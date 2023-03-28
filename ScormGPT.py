9e5ae7d28ee0f4a22b727dc94744e2464de21805
Jude Sajith <judesajith.aj@gmail.com>
2023-03-28 07:31:05 -0400

    jjj

--- ScormGPT.py
+++ ScormGPT.py
@@ -4,13 +4,9 @@ import matplotlib.pyplot as plt
 import mpld3
 from mpld3 import plugins
 import streamlit.components.v1 as components
+import os
 
-# Read data from CSV files
-# df_activity = pd.read_csv('AllLevelActivity_L1 - Sheet 1.csv')
-# df_levelwise_assessment = pd.read_csv('LevelWiseAssement_Level1 - Sheet 1.csv')
-# df_enrollment_metrics = pd.read_csv('EnrollmentMetrics - Sheet 1.csv')
-
-
+# Read data from Excel files
 def read_excel_file(file):
     df = pd.read_excel(file, engine='openpyxl')
     return df
@@ -20,38 +16,46 @@ df_activity_file = st.file_uploader('Upload ALL level activity XLSX file', type=
 df_levelwise_assessment_file = st.file_uploader('Upload Level Wise assessment for level1 XLSX file', type=['xlsx'])
 df_enrollment_metrics_file = st.file_uploader('Upload EnrollmentMetrics XLSX file', type=['xlsx'])
 
-# Read data from uploaded Excel files
-if df_activity_file is not None:
-    df_activity = read_excel_file(df_activity_file)
-    if 'df_activity' not in st.session_state:
-        st.session_state['df_activity'] = df_activity
-
+# If files already exist in the directory, load them
+if os.path.isfile('AllLevelActivity_L1.xlsx'):
+    df_activity = pd.read_excel('AllLevelActivity_L1.xlsx', engine='openpyxl')
+    st.session_state['df_activity'] = df_activity
 else:
-    df_activity = pd.DataFrame() 
-    
-if df_levelwise_assessment_file is not None:
-    df_levelwise_assessment = read_excel_file(df_levelwise_assessment_file)
-    if 'df_levelwise_assessment' not in st.session_state:
-        st.session_state['df_levelwise_assessment'] = df_levelwise_assessment
+    df_activity = pd.DataFrame()
+
+if os.path.isfile('LevelWiseAssesment_Level1.xlsx'):
+    df_levelwise_assessment = pd.read_excel('LevelWiseAssesment_Level1.xlsx', engine='openpyxl')
+    st.session_state['df_levelwise_assessment'] = df_levelwise_assessment
 else:
     df_levelwise_assessment = pd.DataFrame()
 
-if df_enrollment_metrics_file is not None:
-    df_enrollment_metrics = read_excel_file(df_enrollment_metrics_file)
-    if 'df_enrollment_metrics' not in st.session_state:
-        st.session_state['df_enrollment_metrics'] = df_enrollment_metrics
+if os.path.isfile('df_enrollment_metrics.xlsx'):
+    df_enrollment_metrics = pd.read_excel('df_enrollment_metrics.xlsx', engine='openpyxl')
+    st.session_state['df_enrollment_metrics'] = df_enrollment_metrics
 else:
     df_enrollment_metrics = pd.DataFrame()
+
+# Read data from uploaded Excel files
+if df_activity_file is not None:
+    df_activity = read_excel_file(df_activity_file)
+    df_activity.to_excel('df_activity.xlsx', index=False)
+    st.session_state['df_activity'] = df_activity
+
+if df_levelwise_assessment_file is not None:
+    df_levelwise_assessment = read_excel_file(df_levelwise_assessment_file)
+    df_levelwise_assessment.to_excel('df_levelwise_assessment.xlsx', index=False)
+    st.session_state['df_levelwise_assessment'] = df_levelwise_assessment
+
+if df_enrollment_metrics_file is not None:
+    df_enrollment_metrics = read_excel_file(df_enrollment_metrics_file)
+    df_enrollment_metrics.to_excel('df_enrollment_metrics.xlsx', index=False)
+    st.session_state['df_enrollment_metrics'] = df_enrollment_metrics
 
 try:
     df_activity['hours'] = pd.to_datetime(df_activity.iloc[:, 9]) - pd.to_datetime(df_activity.iloc[:, 9]).min()
     df_activity['hours'] = df_activity['hours'].dt.total_seconds() / 3600
-    # df_activity['hours']  =  df_activity['hours'].dt.total_seconds() / 3600
     average_time_spent =  df_activity['hours'].mean()
     st.write(average_time_spent)
-except KeyError:
-    st.warning("Upload Files to View Analytics")
-
 
 
 
@@ -81,6 +85,8 @@ col3.metric(label='Average Time Spent', value=average_time_spent, delta="7%")
 st.subheader('Total Attempts by Module')
 components.html(html_graph, height=600)
 
+except KeyError:
+    st.warning("Upload Files to View Analytics")
 
 
 # except KeyError:
