5c9f723d9a2cde85db34dc3c6a101515b662a587
Jude Sajith <judesajith.aj@gmail.com>
2023-03-28 00:44:49 -0400

    ssss

--- ScormGPT.py
+++ ScormGPT.py
@@ -11,7 +11,6 @@ df_levelwise_assessment = pd.read_csv('LevelWiseAssement_Level1 - Sheet 1.csv')
 df_enrollment_metrics = pd.read_csv('EnrollmentMetrics - Sheet 1.csv')
 
 df_activity['Total_Time_Spent'] = pd.to_timedelta(df_activity['Total_Time_Spent'])
-
 # Calculate performance metrics
 total_learners = len(df_enrollment_metrics)
 total_attempts = df_activity['Total_No_Of_Attempts'].sum()
@@ -27,11 +26,13 @@ plt.title('Total Attempts by Module')
 # Convert the plot to an interactive chart using mpld3
 html_graph = mpld3.fig_to_html(fig, template_type="simple")
 
+activityhrs = df['Total_Time_Spent'].dt.total_seconds() / 3600
+ahrs = activityhrs.mean()
 # Create the streamlit app
 st.title('Overall Performance Dashboard')
 st.subheader('Performance Metrics')
 st.metric(label='Total Number of Learners:', value=total_learners)
 st.metric(label='Total Number of Attempts:', value=total_attempts)
-st.metric(label='Average Time Spent', value=average_time_spent)
+st.metric(label='Average Time Spent', value=ahrs)
 st.subheader('Total Attempts by Module')
 components.html(html_graph)
