e65d4b460b242663ccdd257a5595a827084439be
Jude Sajith <judesajith.aj@gmail.com>
2023-03-29 08:33:01 -0400

    sssss

--- pages/Predictive_Analytics.py
+++ pages/Predictive_Analytics.py
@@ -30,3 +30,26 @@ if essay:
     st.code(questions, language=None)
 
 
+# typeques= st.selectbox("Select the type of questions you want to generate",['essay','mcq'])
+scheme = st.text_area("Paste the marking scheme here")
+answers = st.text_area("Paste the answers given by student here")
+result = st.button("Grade the student")
+# grade = st.button("Grade result")
+
+
+if result:
+    promp = "Use the criteria in the given marking scheme, and grade the answers given by the student according to the scheme, list down the areas the student is weak based on the grades .\n Marking scheme : " + str(scheme) +"\n" + "Student answers :" +"\n"+  str(answers)
+    outs = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=promp,
+        temperature=0.56,
+        max_tokens=2100,
+        top_p=1,
+        frequency_penalty=0.35,
+        presence_penalty=0
+    )
+    grading = outs.choices[0].text.strip()
+    st.write(grading)
+
+
+
